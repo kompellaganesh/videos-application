fd46fec67bb9bd84bef733a15090002cab333ec5
GKOMPE-BLRM20 <ganesh.kompella@eagleview.com>
2022-05-02 02:48:50 +0530

    video streaming basic commit

--- src/MoviePage.js
+++ src/MoviePage.js
@@ -6,14 +6,15 @@ export default function MoviePage() {
   const search = useLocation().search;
   const moviename = new URLSearchParams(search).get('moviename');
   const moviepath = new URLSearchParams(search).get('moviepath');
-
+    const urlstream = 'http://0.0.0.0:80/videos/api/v1/streamvideo?video='+moviepath
+    console.log(urlstream)
   return (
     <div>
       <h1>MoviePage page</h1>
       <p>{moviename}</p>
       <p>{moviepath}</p>
       <video controls muted autoPlay crossOrigin="anonymous">
-        <source src={moviepath} type="mkv"></source>
+        <source src={urlstream} type="video/mp4"></source>
         {/* <track label="English" kind="captions" srcLang="en" src={moviepath} default></track> */}
     </video>
     </div>
